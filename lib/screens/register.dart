52f6632a80c99d8494e521b8297b78fe263ce77e
Ajay Sankar <ajay0196@gmail.com>
2020-10-05 16:46:52 -0700

    Enclosed in a SingleChildScrollView widget

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:assignment_2/utils/input_decoration.dart';
 import 'package:assignment_2/utils/login_register_buttons.dart';
+import 'package:assignment_2/utils/validators.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -31,70 +32,73 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
+
+    var register = () {
+      final form = formKey.currentState;
+      if (form.validate()) {
+        print("All good");
+      }
+    };
+
     return SafeArea(
         child: Scaffold(
-            body: Container(
-                padding: EdgeInsets.all(40.0),
-                child: Form(
-                    key: formKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
-                          child: Text(
-                              "Register",
-                              style: TextStyle(
-                                  fontSize: 40,
-                                  color: Color(0xfff063057)
-                              )
-                          ),
-                        ),
-                        SizedBox(height: 25.0),
-                        TextFormField(
-                          autofocus: false,
-                          validator: (value) => value.isEmpty ? "Please enter name" : null,
-                          onSaved: (value) => setState(() { _userName = userNameController.text; }),
-                          decoration: buildInputDecoration("Name", Icons.account_circle),
-                        ),
-                        SizedBox(height: 20.0),
-                        TextFormField(
-                          controller: nickNameController,
-                          validator: (value) => value.isEmpty ? "Please enter nickname" : null,
-                          autofocus: false,
-                          onSaved: (value) => setState(() { _nickName = nickNameController.text; }),
-                          decoration: buildInputDecoration("Nick Name", Icons.account_circle),
-                        ),
-                        SizedBox(height: 20.0),
-                        TextFormField(
-                          controller: passwordController,
-                          autofocus: false,
-                          obscureText: true,
-                          validator: (value) => value.isEmpty ? "Please enter password" : null,
-                          onSaved: (value) => setState(() { _password = passwordController.text; }),
-                          decoration: buildInputDecoration("Password", Icons.lock),
-                        ),
-                        SizedBox(height: 20.0),
-                        TextFormField(
-                          controller: confirmPasswordController,
-                          autofocus: false,
-                          obscureText: true,
-                          validator: (value) => value.isEmpty ? "Please enter confirm password" : null,
-                          onSaved: (value) => setState(() { _confirmPassword = confirmPasswordController.text; }),
-                          decoration: buildInputDecoration("Confirm Password", Icons.lock),
-                        ),
-                        SizedBox(height: 20.0),
-                        TextFormField(
-                          controller: emailController,
-                          autofocus: false,
-                          validator: (value) => value.isEmpty ? "Please enter email" : null,
-                          onSaved: (value) => setState(() { _email = emailController.text; }),
-                          decoration: buildInputDecoration("Email", Icons.email),
-                        ),
-                        SizedBox(height: 20.0),
-                        longButtons("Register", () => {})
-                      ],
+            body: Center(
+              child: SingleChildScrollView(
+                child: Container(
+                    padding: EdgeInsets.all(40.0),
+                    child: Form(
+                        key: formKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: Text(
+                                  "Register",
+                                  style: TextStyle(
+                                      fontSize: 40,
+                                      color: Color(0xfff063057)
+                                  )
+                              ),
+                            ),
+                            SizedBox(height: 25.0),
+                            TextFormField(
+                              autofocus: false,
+                              validator: (value) => value.isEmpty ? "Please enter name" : null,
+                              onSaved: (value) => setState(() { _userName = userNameController.text; }),
+                              decoration: buildInputDecoration("Name", Icons.account_circle),
+                            ),
+                            SizedBox(height: 20.0),
+                            TextFormField(
+                              controller: nickNameController,
+                              validator: (value) => value.isEmpty ? "Please enter nickname" : null,
+                              autofocus: false,
+                              onSaved: (value) => setState(() { _nickName = nickNameController.text; }),
+                              decoration: buildInputDecoration("Nick Name", Icons.account_circle),
+                            ),
+                            SizedBox(height: 20.0),
+                            TextFormField(
+                              controller: passwordController,
+                              autofocus: false,
+                              obscureText: true,
+                              validator: (value) => value.isEmpty ? "Please enter password" : null,
+                              onSaved: (value) => setState(() { _password = passwordController.text; }),
+                              decoration: buildInputDecoration("Password", Icons.lock),
+                            ),
+                            SizedBox(height: 20.0),
+                            TextFormField(
+                              controller: emailController,
+                              autofocus: false,
+                              validator: validateEmail,
+                              onSaved: (value) => setState(() { _email = emailController.text; }),
+                              decoration: buildInputDecoration("Email", Icons.email),
+                            ),
+                            SizedBox(height: 20.0),
+                            longButtons("Register", register)
+                          ],
+                        )
                     )
-                )
+                ),
+              ),
             )
         )
     );

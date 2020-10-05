ec306e8d3e7ef0f7a492527f012b95480b5df5c4
Ajay Sankar <ajay0196@gmail.com>
2020-10-05 13:23:16 -0700

    Added login widget

---
+++ lib/screens/login.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:assignment_2/utils/input_decoration.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final formKey = new GlobalKey<FormState>();
+
+  String _nickname, _password;
+  TextEditingController nickNameController;
+  TextEditingController passwordController;
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    nickNameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(40.0),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 15.0),
+                    TextFormField(
+                      controller: nickNameController,
+                      validator: (value) => value.isEmpty ? "Please enter nickname" : null,
+                      autofocus: false,
+                      onSaved: (value) => setState(() { _nickname = nickNameController.text; }),
+                      decoration: buildInputDecoration("Nick Name", Icons.account_circle),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      controller: passwordController,
+                      autofocus: false,
+                      obscureText: true,
+                      validator: (value) => value.isEmpty ? "Please enter password" : null,
+                      onSaved: (value) => setState(() { _password = passwordController.text; }),
+                      decoration: buildInputDecoration("Password", Icons.lock),
+                    ),
+                    SizedBox(height: 20.0)
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

4ed50f1a44238eb5e8dd485c677acd4639fc01f2
Ajay Sankar <ajay0196@gmail.com>
2020-10-05 16:45:46 -0700

    Added reference

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -1,12 +1,13 @@
+// Reference - https://medium.com/@aslamanver/email-validation-in-dart-flutter-e1f3264ab59d
+
 String validateEmail(String value) {
   String _msg;
   RegExp regex = new RegExp(
       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
   if (value.isEmpty) {
-    _msg = "Your username is required";
+    _msg = "Your email is required";
   } else if (!regex.hasMatch(value)) {
     _msg = "Please provide a valid emal address";
   }
-  print(_msg);
   return _msg;
 }

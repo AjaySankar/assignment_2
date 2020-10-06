c7c06a31ebe5c8b8be43353f3a551f7e6c252892
Ajay Sankar <ajay0196@gmail.com>
2020-10-06 11:54:04 -0700

    Added Create Post From widget

---
+++ lib/screens/post_create_form.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:assignment_2/utils/validators.dart';
+
+enum Status {
+  PostNotCreated,
+  UploadingPost,
+  PostUploadSuccess,
+  PostUploadFailed
+}
+
+class PostForm extends StatefulWidget {
+  @override
+  _PostFormState createState() => _PostFormState();
+}
+
+class _PostFormState extends State<PostForm> {
+  final formKey = new GlobalKey<FormState>();
+  String _postDescription;
+  TextEditingController postDescriptionController = TextEditingController(text: '');
+
+  @override
+  void dispose() {
+    postDescriptionController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Text(
+                    "Create Post",
+                    style: TextStyle(
+                        fontSize: 40,
+                        color: Color(0xfff4267B2)
+                    )
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Container(
+                  margin: const EdgeInsets.only(left: 10.0, right: 20.0),
+                  child: TextFormField(
+                    controller: postDescriptionController,
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 10,
+                    maxLength: 144,
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Ink(
+                  decoration: const ShapeDecoration(
+                    color: const Color(0xfff4267B2),
+                    shape: CircleBorder(),
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_forward_ios),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}

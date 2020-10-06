c522e3bb4071e29c4649c042b9c06e65fe83813c
Ajay Sankar <ajay0196@gmail.com>
2020-10-05 22:40:27 -0700

    Added urls module

---
+++ lib/utils/urls.dart
@@ -0,0 +1,7 @@
+// URLs used in the InstaPost App
+
+class Urls {
+  static const String queryBaseURL = "https://bismarck.sdsu.edu/api/instapost-query";
+  static const String uploadBaseURL = "https://bismarck.sdsu.edu/api/instapost-upload";
+  static const String register = uploadBaseURL + "/newuser";
+}

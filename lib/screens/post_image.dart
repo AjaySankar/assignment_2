b84db8246883193b54ed0eac5b4ec2e167db3f68
Ajay Sankar <ajay0196@gmail.com>
2020-10-07 20:19:32 -0700

    Fetch image and show in image post..

--- lib/screens/post_image.dart
+++ lib/screens/post_image.dart
@@ -1,16 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:assignment_2/insta_post_requests/getPostImage.dart';
+import 'package:assignment_2/utils/request_states.dart';
+import 'package:assignment_2/utils/image_io.dart';
 
 class PostImage extends StatefulWidget {
-  PostImage(this.postId);
-  final int postId;
+  PostImage(this.imageId);
+  final int imageId;
 
   @override
   _PostImageState createState() => _PostImageState();
 }
 
 class _PostImageState extends State<PostImage> {
+
+  GetPostImage getPostImageHandle;
+
+  @override
+  void initState() {
+    getPostImageHandle = GetPostImage((Status requestState) => {});
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    var placeHolderImage = (IconData icon, [Color color = Colors.blue]) {
+      return Expanded(
+        child: new FittedBox(
+          fit: BoxFit.fitHeight,
+          child: Icon(
+            icon,
+            color: color,
+            size: 60,
+          ),
+        )
+      );
+    };
+
+    var getImageFromResponse = (response) {
+      if(response['status']) {
+        final base64EncodedImage = response['body']['image'];
+        return Expanded(
+          child: Image.memory(
+            base64Decode(base64EncodedImage),
+            fit: BoxFit.fitHeight,
+          ),
+        );
+      }
+      return placeHolderImage(Icons.broken_image, Colors.red);
+    };
+
+    if(widget.imageId == -1) {
+      return placeHolderImage(Icons.image);
+    }
+
+    Future<Map<String, dynamic>> _fetchPostImage = getPostImageHandle.getInstaPost(widget.imageId);
+    return FutureBuilder<Map<String, dynamic>>(
+        future: _fetchPostImage,
+        builder: (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+          Widget image;
+          if(snapshot.hasData) {
+            image = getImageFromResponse(snapshot.data);
+          }
+          else if(snapshot.hasError) {
+            print(snapshot.data);
+            image = placeHolderImage(Icons.broken_image, Colors.red);
+          }
+          else {
+            image = SizedBox(
+              child: CircularProgressIndicator(),
+              width: 60,
+              height: 60,
+            );
+          }
+          return image;
+        }
+    );
     return Expanded(
       child: FittedBox(
         fit: BoxFit.fitWidth, // otherwise the logo will be tiny

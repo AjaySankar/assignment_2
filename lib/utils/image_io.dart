3800380030ff558daeb78c0c3af077755bd7ae95
Ajay Sankar <ajay0196@gmail.com>
2020-10-07 20:18:14 -0700

    Added base64 image decoder..

--- lib/utils/image_io.dart
+++ lib/utils/image_io.dart
@@ -3,7 +3,11 @@
 import 'dart:io';
 import 'dart:convert';
 
+import 'dart:typed_data';
+
 String readImageAsBase64Encode(File image) {
   final bytes = image.readAsBytesSync();
   return base64Encode(bytes);
 }
+
+Uint8List base64Decode(String source) => base64.decode(source);

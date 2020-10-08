9e8978ab57f6db2686cbdcb4b631e72670dc0ce8
Ajay Sankar <ajay0196@gmail.com>
2020-10-07 21:10:27 -0700

    Added star rating handler..

--- lib/screens/rating_stars.dart
+++ lib/screens/rating_stars.dart
@@ -1,8 +1,11 @@
 //https://pub.dev/packages/smooth_star_rating
 
+import 'package:assignment_2/insta_post_requests/ratePost.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
 import 'package:assignment_2/utils/theme.dart';
+import 'package:assignment_2/utils/request_states.dart';
+import 'package:assignment_2/utils/snackBar.dart';
 
 class RatingStars extends StatefulWidget {
   RatingStars(this.postId);
@@ -13,15 +16,38 @@ class RatingStars extends StatefulWidget {
 }
 
 class _RatingStarsState extends State<RatingStars> {
-  var rating = 3.0;
+
+  RatePost ratePostHandle;
+  var rating = 0.0;
+  bool isReadOnly = false; // Enabled user rating initially.
+
+  @override
+  void initState() {
+    ratePostHandle = RatePost((Status requestState) => {});
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    var afterRating = (Map<String, dynamic> response) {
+      print(response);
+      if(!response['status']) {
+        showSnackBar(response['message']??'Failed to rate post. Try again !!', context);
+      }
+      else {
+        // Disable rating once user has successfully rated.
+        setState(() {
+          isReadOnly = true;
+        });
+      }
+    };
+
     return SmoothStarRating(
-      color: getThemeColor(),
-      borderColor: getThemeColor(),
+      color: isReadOnly ? Colors.green : getThemeColor(),
+      borderColor: isReadOnly ? Colors.green : getThemeColor(),
       rating: rating,
-      isReadOnly: false,
+      isReadOnly: isReadOnly,
       size: 20,
       filledIconData: Icons.star,
       halfFilledIconData: Icons.star_half,
@@ -30,8 +56,9 @@ class _RatingStarsState extends State<RatingStars> {
       allowHalfRating: false,
       spacing: 2.0,
       onRated: (value) {
-        print("rating value -> $value");
-        // print("rating value dd -> ${value.truncate()}");
+        print("rating value -> ${value.round()}");
+        ratePostHandle.rate(widget.postId, value.round())
+            .then(afterRating);
       },
     );
   }

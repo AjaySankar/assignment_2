49822510c8523cd3fe3c36a3dd43db6ef5b11620
Ajay Sankar <ajay0196@gmail.com>
2020-10-08 13:27:51 -0700

    renamed progress bar..

--- lib/utils/circularProgress.dart
+++ lib/utils/circularProgress.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-var getCircularProgress = ([width = 60, height = 60]) {
+var getCircularProgress = ([double width = 60, double height = 60]) {
   return SizedBox(
     child: CircularProgressIndicator(),
     width: width,

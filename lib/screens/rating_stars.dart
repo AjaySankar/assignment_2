47467bfd22daa4b37646b79f57d06a4de86fe035
Ajay Sankar <ajay0196@gmail.com>
2020-10-07 20:20:00 -0700

    Added pubspec reference..

--- lib/screens/rating_stars.dart
+++ lib/screens/rating_stars.dart
@@ -1,3 +1,5 @@
+//https://pub.dev/packages/smooth_star_rating
+
 import 'package:flutter/material.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
 import 'package:assignment_2/utils/theme.dart';

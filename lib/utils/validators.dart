21061c9a6405aabdda86c19f9100608fcc860e75
Ajay Sankar <ajay0196@gmail.com>
2020-10-17 17:48:05 -0700

    Seperated out logic to check if email format is valid..

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -4,14 +4,21 @@ const int MIN_HASH_TAG_LEN = 2; // Including # symbol
 
 String validateEmail(String value) {
   String _msg;
-  RegExp regex = new RegExp(
-      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
   if (value.isEmpty) {
     _msg = "Your email is required";
-  } else if (!regex.hasMatch(value)) {
+  } else if (!isEmailValidFormat(value)) {
     _msg = "Please provide a valid emal address";
   }
   return _msg;
+}
+
+bool isEmailValidFormat(String email) {
+  if (email.length == 0) {
+    return false;
+  }
+  RegExp regex = new RegExp(
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+  return regex.hasMatch(email);
 }
 
 String validatePassword(String value) {
